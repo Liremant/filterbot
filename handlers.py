6bcd1215078cb8a5f84206be762111d0e5cf34aa
Liremant <mail.liremant@gmail.com>
2025-11-18 22:34:10 +0800

    fixed regex trigger(not regex now,only definetly match words)

--- handlers.py
+++ handlers.py
@@ -17,15 +17,18 @@ async def bot_message_handler(message: Message):
 @dp.message(IsUser(), IsChat(), IsMessageFromFilterGroup(FILTER_GROUP))
 async def regex_uesr_handler(message: Message):
     print("handler triggered")
-    if message.text in regex["expressions"]:
-        print("regex triggered")
+    print(f"this is regex:{regex['expressions']}")
+    print(f"{message.text.unwrap()}")
+
+    if message.text.unwrap() in regex["expressions"]:
+        print("regex detected!")
         await message.api.send_message(
             chat_id=ADMIN_GROUP,
             text=(
                 "Sussy user detected!\n\n"
                 f"From: {message.from_user.full_name}\n"
-                f"Username: @{message.from_user.username}\n"
+                f"Username: @{message.from_user.username.unwrap()}\n"
                 f"ID: {message.from_user.id}\n\n"
-                f"Message: {message.text}"
+                f"Message: {message.text.unwrap()}"
             ),
         )
